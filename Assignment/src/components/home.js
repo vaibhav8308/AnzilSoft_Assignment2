6bd617846a9222cf7a231453953ae32d1dc84aae
vaibhav8308 <vaibhav.maske1997@gmail.com>
2022-03-22 17:18:31 +0530

    added successfully

--- Assignment/src/components/home.js
+++ Assignment/src/components/home.js
@@ -1,28 +1,41 @@
 import React from 'react'
 import './Home.css'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Input from './input'
+
 
 function Home() {
 
     const [addName, setAddName] = useState([])
 
+
+
     const tableData = addName.map((ele) => { //  function to add data to tsble row
         return (
-            <tr>
+            <tr  >
                 <td>{ele.firstName}</td>
                 <td>{ele.lastName}</td>
             </tr>
         )
     })
-    
+    useEffect(() => {
+        setAddName(JSON.parse(localStorage.getItem('addName')));
+    }, []);
+
+    useEffect(() => {
+        (localStorage.setItem('addName', JSON.stringify(addName)));
+    }, [addName]);
+
 
     const newRow = (List) => {     // function to add new row
- 
+
         const updateList = [...addName]
         updateList.push(List)
         setAddName(updateList)
     }
+
+
+
 
     return (
         <div className='output'>
